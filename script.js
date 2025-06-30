bc025d5dc461d7760bb85bb2ce2788460f786add
zire98 <164895719+zire98@users.noreply.github.com>
2025-06-30 12:50:34 +0200

    Added functions and events.
    We already have the function to clear the grid, and to create it, in addition to the constants and events to manage the grid created by promt and the default 16x16.

---
+++ script.js
@@ -0,0 +1,41 @@
+// Const
+const DEF_SIZE = 16;
+const MAX_SIZE = 100;
+const GRID_WIDTH = 960;
+
+let grid;
+
+// Functions
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+function createGrid(size) {
+    clearGrid();
+    const squareSize = GRID_WIDTH / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        grid.appendChild(square);
+    }
+}
+
+// Events
+document.addEventListener("DOMContentLoaded", () => {
+    grid = document.querySelector("#container");
+    const button = document.querySelector("#buttonSize");
+
+    button.addEventListener("click", () => {
+        let newSize = parseInt(prompt(`Enter new grid size (max ${MAX_SIZE})`));
+        if (newSize > 0 && newSize <= MAX_SIZE) {
+            createGrid(newSize);
+        } else {
+            alert(`Invalid input. Please enter a number between 1 and ${MAX_SIZE}`);
+        }
+    });
+    createGrid(DEF_SIZE);
+});
